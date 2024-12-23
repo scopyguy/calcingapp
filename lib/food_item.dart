4387202cf1e201dc774c7c6602be030711fb7f67
scopyguy <adeniyi20215@gmail.com>
2024-12-23 07:26:54 -0800

    calcingapp

---
+++ lib/food_item.dart
@@ -0,0 +1,6 @@
+Map<String, dynamic> deliveries = {
+  'food_item': ['Fuits', 'Vegetables', 'Proteins', 'Grains', 'Diary', 'Snacks'],
+  'name_of_food': ['Chicken', 'Banana', 'Bread', 'Rice', 'Juice'],
+  'location_of_delivery': ['Oyo', 'Iseyin', 'Ibadan', 'Saki'],
+  'quantity': ['2, 1, 5, 8,9'],
+};
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:calcingapp/body.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Order();
+  }
+}
